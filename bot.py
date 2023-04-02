c47808dd61ba1ec39c99f31a230dd74543edba9f
unknown <monshinawatra@gmail.com>
2023-04-02 21:35:38 +0700

    refactoring code

--- bot.py
+++ bot.py
@@ -1,61 +1,46 @@
 import os
+from enum import Enum
 
 import discord
-from discord.ext import tasks, commands
-import responses
-import scheduler
+from discord import app_commands
+from discord.ext import commands, tasks
 
-
-
-# Send messages
-async def send_message(message, user_message, is_private):
-    try:
-        response = responses.handle_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-
-    except Exception as e:
-        print(e)
+from utils import *
 
 
 def run_discord_bot():
     intents = discord.Intents.default()
     intents.message_content = True
-    client = discord.Client(intents=intents)
-    # bot = commands.Bot(command_prefix='/', intents=intents)
+    bot = commands.Bot(command_prefix="/", intents=intents)
 
-    @tasks.loop(hours=24)
-    async def update():
-        # await send_message("update", scheduler.update_status(), is_private=True)
-        channel = client.get_channel(1091281973441482814)
-        status = scheduler.update_status()
-        if status:
-            await channel.send(status)
-
-    @client.event
+    @bot.event
     async def on_ready():
-        print(f'{client.user} is now running!')
-        update.start()
+        print(f"Bot is Up and Ready to Go! {bot.user}")
+        try:
+            synced = await bot.tree.sync()
+            print(f"Synced {synced} commands")
+        except Exception as e:
+            print(e)
 
-    @client.event
-    async def on_message(message):
-        # Make sure bot doesn't get stuck in an infinite loop
-        if message.author == client.user:
-            return
+    @bot.hybrid_group(fallback="list")
+    async def classes(ctx):
+        classes = read_class_info()
+        header = "Available Classes: \n"
+        texts = header
+        for index, row in classes.iterrows():
+            texts += f"`{row['id']}` - {row['name']} by {row['lecturer']}\n"
+        if texts == header:
+            texts = "No classes available"
+        await ctx.send(texts, ephemeral=True)
 
-        # Get data about the user
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
+    @classes.command()
+    async def add(ctx, id: str, name: str, lecturer: str):
+        return_message = add_class_info(id, name, lecturer)
+        await ctx.send(return_message)
 
-        # Debug printing
-        print(f"{username} said: '{user_message}' ({channel})")
+    @classes.command()
+    async def remove(ctx, id: str):
+        return_message = remove_class_info(id)
+        await ctx.send(return_message)
 
-        # If the user message contains a '?' in front of the text, it becomes a private message
-        if user_message[0] == '?':
-            user_message = user_message[1:]  # [1:] Removes the '?'
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
-    # Remember to run your bot with your personal TOKEN
-    
-    client.run(os.getenv("BETTERLEB2_BOT_TOKEN"))
+    bot.run(os.getenv("BETTERLEB2_BOT_TOKEN"))
